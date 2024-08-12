1cb4da18e384c37d9702de04602390af0d67d36d
Ninglo <jiujianian@gmail.com>
2024-08-12 12:27:27 +0800

    Sync to remote

--- scripts/circle/tag/putTag.ts
+++
@@ -1,5 +0,0 @@
-// import users from "result.json" with { type: "json" };
-
-// const list = users.records.filter(record => !record.member_tags.length).map(record => record.id)
-
-// console.log(list.join(','))
